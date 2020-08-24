988073fe11959886bcc6682994e3421a13e6aba7
Sebastian Viasus <vseb85@gmail.com>
2020-08-24 13:47:44 -0400

    Finished Rails Stupid Coaching

---
+++ app/controllers/responses_controller.rb
@@ -0,0 +1,12 @@
+class ResponsesController < ApplicationController
+    def answer 
+
+        if params[:question] == "I am going to work"
+            @answer = "Great!"
+        elsif params[:question].last == "?"
+            @answer = "Silly question, get dressed and go to work!"
+        else 
+            @answer = "I don't care, get dressed and go to work!"
+        end 
+    end 
+end
